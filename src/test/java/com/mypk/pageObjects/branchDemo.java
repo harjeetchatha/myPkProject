25681ff1c8856428ffe3d2c597577b72b5c90691
hchatha <hchatha@hchatha-pk.hsd1.ca.comcast.net>
2019-08-27 01:06:32 -0700

    branch

---
+++ src/test/java/com/mypk/pageObjects/branchDemo.java
@@ -0,0 +1,15 @@
+package com.mypk.pageObjects;
+
+public class branchDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String h = "Hello";
+		String b = h;
+		
+		System.out.println(b);
+
+	}
+
+}
